d1ceeea04ca1066e742edf383eec13950522b659
Ante Wall <antewall@gmail.com>
2020-03-12 15:36:33 +0100

    Fix unitialized variable

--- src/lambda/truth.js
+++ src/lambda/truth.js
@@ -5,6 +5,8 @@ dotenv.config();
 const token = process.env.SLACK_TOKEN;
 const web = new WebClient(token);
 
+const PROBABILITY = 0.5; // 50%
+
 exports.handler = async (event, context) => {
   // Only allow POST
   if (event.httpMethod !== "POST") {
@@ -14,7 +16,8 @@ exports.handler = async (event, context) => {
   // When the method is POST, the name will no longer be in the event’s
   // queryStringParameters – it’ll be in the event body encoded as a queryString
   const params = querystring.parse(event.body);
-  const res = await web.chat.postMessage({
+  const truth = Math.random() >= PROBABILITY;
+  return await web.chat.postMessage({
     channel: params.channel,
     text: `Ante talar... ${truth ? "Sanning!" : "Inte Sanning!"}`
   });
